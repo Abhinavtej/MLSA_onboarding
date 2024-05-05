96d22410a37f0c8f480eb3b03032d6be95f0cf7e
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2024-05-05 22:08:57 +0530

    commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Useful Tools - Web apps",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed using HTML, CSS & Js. Integrated with APIs",
+    url: "https://github.com/Abhinavtej/Free-Tools",
   },
   {
-    title: "Web Development for Beginners",
+    title: "NavAsssist",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "An Indoor Navigation Web app especially designed for my university.",
+    url: "https://indoor-navigation.tiiny.site/?currentLocation=Entrance",
   },
   {
-    title: "My Resume Site",
+    title: "My Portfolio Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created with React Js",
+    url: "https://abhinavtej.vercel.app",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "MyPocket - Budget Organiser for Students",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Desktop app completely build with Python and its frameworks (Pyside6)",
+    url: "https://github.com/Abhinavtej/MyPocket",
   },
 ];
 
